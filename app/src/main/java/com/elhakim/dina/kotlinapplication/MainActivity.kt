d1cf13fd64cca8e82fa0a316f85d63608c5fb2b8
dinaelhakim <dina.abdelmoneim-elhakim@vodafone.com>
2018-10-15 12:43:14 +0200

    create simple activity with kotlin

---
+++ app/src/main/java/com/elhakim/dina/kotlinapplication/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.elhakim.dina.kotlinapplication
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private var intVar = 9
+    private var floatVar: Float = 0F
+    private var strVar = "Hello"
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val textView: View = findViewById(R.id.text_view)
+
+        floatVar = intVar.toFloat()  //explicit casting
+
+        if (textView is TextView) {    //smart casting
+            textView.text = String.format(getString(R.string.test_placeholder_str), strVar, floatVar)
+        }
+    }
+}
